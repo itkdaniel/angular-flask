41c3865c7b590b5057acd2869d8f2ea6f7d5c8ac
Daniel Truong <daktruon@ucsc.edu>
2023-05-18 04:47:39 -0700

    added Forms Modules

--- services/frontend/src/app/app.module.ts
+++ services/frontend/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExamListComponent } from './exam-list/exam-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ExamsApiService } from './services/exams-api.service';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AddExamComponent } from './add-exam/add-exam.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExamListComponent
+    ExamListComponent,
+    HomePageComponent,
+    AddExamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [ExamsApiService],
   bootstrap: [AppComponent]
