4cdb859dc890b75a618455bc243cb8b2d5cdbf69
Daniel Truong <daktruon@ucsc.edu>
2023-06-09 04:19:03 -0700

    added postgres test database

--- services/db/init.sql
+++ services/db/init.sql
@@ -1,1 +1,3 @@
 SELECT 'CREATE DATABASE online_exam' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'online_exam')\gexec
+
+SELECT 'CREATE DATABASE online_exam_test' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'online_exam_test')\gexec
