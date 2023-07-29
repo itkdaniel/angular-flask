2788f8fcb6c86a1fdfa1ce820647b4aff5d73816
Daniel Truong <daktruon@ucsc.edu>
2023-07-29 01:24:59 -0700

    updated chatgpt service session management

--- services/api/src/api/chatgpt.py
+++ services/api/src/api/chatgpt.py
@@ -12,18 +12,18 @@ from enum import Enum
 import json
 # API-endpoint:  https://free.churchless.tech/v1/chat/completions
 # API-key: BetterChatGPT
-
+ # https://free.churchless.tech/v1/chat/completions
 gptlogger = logger.create_logger('GptLogger')
 better_chatgpt_api_endpoint = ' https://free.churchless.tech/v1/chat/completions'
 api_key = 'BetterChatGPT'
 headers = {'Content-Type':'application/json',
 			'Authorization': f'Bearer {api_key}'}
-
+model = "gpt-4"
 chatgpt_blueprint = Blueprint('chatgpt', __name__)
 
 
 messages = [{'role': "😀",'content': "😵"}]
-data = {'model':'gpt-3.5-turbo','n':2,'messages':messages}
+data = {'model':model,'n':2,'messages':messages}
 
 @chatgpt_blueprint.route('/api/gpt/ping',methods=['GET','POST'])
 def pingpong():
@@ -86,31 +86,31 @@ def fetch_message():
 	role = Enum('role','user assistant system',module=__name__,qualname=f'{__name__}.role')
 	text_response = make_dataclass('TextResponse',[('role',role),('sno',int,1),('text',str,''),('response',str,'')],namespace={'todict':lambda self:asdict(self,dict_factory=dict)})
 	response = {'status':'failed'}
-	json_payload = session['chathistory'] if 'chathistory' in session else {"model":"gpt-3.5-turbo",
-																			"n":2,
-																			"messages":[]}
 	try:
 		# use default headers for now
 		json_data = request.get_json()
 		json_data = text_response(**json_data)
 		message = {'role':role[json_data.role].name,
 					'content':json_data.text}
-		json_payload['messages'].append(message)
-		session['chathistory']['messages'].append(message)
+		# if 'chathistory' in session: gptlogger.debug(f"current chathistory: {Colors.GREEN}{session['chathistory']}")
+		session.update({'chathistory':{"model":model,"n":2,"messages":[message]}}) if 'chathistory' not in session else session['chathistory']['messages'].extend([message])
 		session.modified = True
+		json_payload = session['chathistory'] 
 		gpt_response = requests.post(better_chatgpt_api_endpoint,headers=headers,json=json_payload)
 		response_json =  gpt_response.json()
+		gptlogger.debug(f'{Colors.YELLOW}response json: {response_json}{Colors.DEFAULT}')
 		response_json = ChatGPT(response_json)
 		response_content = response_json.choices[0]['message']['content']
 		response_role = response_json.choices[0]['message']['role']
 		response_message = {'role':role[response_role].name,'content':response_content}
 		# json_payload['messages'].append(response_message)
-		session['chathistory']['messages'].append(response_message) #json_payload
+		session['chathistory']['messages'].extend([response_message]) #json_payload
 		session.modified = True
 		json_data.response = response_content
+		json_data.role = response_role
 		response = json_data.todict()
-		gptlogger.debug(f'{Colors.LIGHT_PURPLE}{200}-{response}{Colors.DEFAULT}')
-		if 'chathistory' in session: gptlogger.debug(f"{Colors.LIGHT_BLUE}fksession-ch - {session['chathistory']} {Colors.DEFAULT}")# else gptlogger.warning(f'fksession-ch - 😵')
+		# gptlogger.debug(f'{Colors.LIGHT_PURPLE}{200}-{response}{Colors.DEFAULT}')
+		if 'chathistory' in session: gptlogger.debug(f"{Colors.LIGHT_BLUE}fksession-ch - {json.dumps(session['chathistory'],indent=3)} {Colors.DEFAULT}")# else gptlogger.warning(f'fksession-ch - 😵')
 		return jsonify(response)
 	except Exception as e:
 		gptlogger.error(f'{Colors.LIGHT_RED}ERROR - {type(e).__name__}{Colors.RED}{e}{Colors.DEFAULT}')
@@ -118,11 +118,26 @@ def fetch_message():
 	raise NotImplementedError('TODO fetch a message from messages storage(mock)\
 								 to feed ChatGpt for priming response 😵')
 
+@chatgpt_blueprint.route('/api/session/test',methods=['GET','POST'])
+def test_flask_session():
+	role = Enum('role','user assistant system',module=__name__,qualname=f'{__name__}.role')
+	text_response = make_dataclass('TextResponse',[('role',role),('sno',int,1),('text',str,''),('response',str,'')],namespace={'todict':lambda self:asdict(self,dict_factory=dict)})
+	if request.method == 'POST':
+		json_data = request.get_json()
+		json_data = text_response(**json_data)
+		message = {'role':role[json_data.role].name,
+					'content':json_data.text}
+		# session['persistent_data'] = 'testing persistence'
+		session.update({'test_chathistory':{"model":model,"n":2,"messages":[message]}}) if 'test_chathistory' not in session else session['test_chathistory']['messages'].extend([message])
+		session.modified = True
+	response = session['test_chathistory'] if 'test_chathistory' in session else 'no chathistory yet - do post first'
+	return jsonify(response)
+
 @chatgpt_blueprint.route('/api/gpt/session/history',methods=['GET','POST'])
 def get_session_history():
-	response = {'message':'session has no saved chathistory'}
-	if 'chathistory' in session:
-		response = session['chathistory']
+	response = {'message':'session has no saved test_chathistory'}
+	if 'test_chathistory' in session:
+		response = session['test_chathistory']
 		return response
 	return response
 
@@ -133,7 +148,7 @@ def test_gpt_post():
 		Endpoint to test requesting completions response
 	"""
 	response = {'status':'failed'}
-	data =  {"model": "gpt-3.5-turbo",
+	data =  {"model": model,
 			"messages": [{"role": "user", "content": "Hello, how are you?"}]}
 	json_data = request.get_json()
 	try:
